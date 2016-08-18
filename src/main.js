f579233d875a0b518d3ceae681c08e23309bacb0
Brandon Allen <firexfighterx@gmail.com>
2016-08-17 22:22:59 -0500

    Renamed Routes file to be uppercase

--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@ var express = require('express');
 var app = express();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
 });
 
-app.use('/api', require('./trucks/truckListRoutes'));
+app.use('/api', require('./trucks/TruckListRoutes'));
 
-app.listen(3000, function () {
-  console.log('Truck Care API listening on port 3000');
+app.listen(3000, function() {
+    console.log('Truck Care API listening on port 3000');
 });

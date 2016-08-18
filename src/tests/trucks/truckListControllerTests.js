eac415bf5def0a472de1066f9b80a3d2d37a7bc6
Brandon Allen <firexfighterx@gmail.com>
2016-08-17 21:58:54 -0500

    Upper-casing file names

--- src/tests/trucks/truckListControllerTests.js
+++ src/tests/trucks/truckListControllerTests.js
@@ -1,32 +1,32 @@
 var assert = require('assert');
 var sinon = require('sinon');
 var truckCareDatabase = require('../../database/truckCareDatabase');
-var truckListCallbackHandlers = require('../../trucks/truckListCallbackHandlers');
-var controller = require('../../trucks/truckListController');
+var truckListCallbackHandlers = require('../../trucks/TruckListCallbackHandlers');
+var controller = require('../../trucks/TruckListController');
 
-describe('truckListController', function(){
-var sandbox;
-  beforeEach(function(){
-    sandbox = sinon.sandbox.create();
-  });
-
-  afterEach(function(){
-    sandbox.restore();
-  });
-
-  describe('getAllTrucks', function(){
-    it('calls truck care database to get all trucks', function(){
-
-      var req = {};
-      var res = {};
-
-      var getAllTrucksHandler = sandbox.stub(truckListCallbackHandlers, 'getAllTrucksHandler');
-      var getItems = sandbox.stub(truckCareDatabase, 'getItems');
-
-      controller.getAllTrucks(req, res);
-
-      assert(getItems.withArgs(getAllTrucksHandler).calledOnce, 'called truck care database get items');
+describe('truckListController', function() {
+    var sandbox;
+    beforeEach(function() {
+        sandbox = sinon.sandbox.create();
     });
-  });
+
+    afterEach(function() {
+        sandbox.restore();
+    });
+
+    describe('getAllTrucks', function() {
+        it('calls truck care database to get all trucks', function() {
+
+            var req = {};
+            var res = {};
+
+            var getAllTrucksHandler = sandbox.stub(truckListCallbackHandlers, 'getAllTrucksHandler');
+            var getItems = sandbox.stub(truckCareDatabase, 'getItems');
+
+            controller.getAllTrucks(req, res);
+
+            assert(getItems.withArgs(getAllTrucksHandler).calledOnce, 'called truck care database get items');
+        });
+    });
 
 });
--- src/trucks/truckListController.js
+++ src/trucks/truckListController.js
@@ -1,9 +1,9 @@
 var truckCareDatabase = require('../database/truckCareDatabase');
-var truckListCallbackHandlers = require('../trucks/truckListCallbackHandlers');
+var truckListCallbackHandlers = require('../trucks/TruckListCallbackHandlers');
 var controller = {};
 
-controller.getAllTrucks = function(req, res){
-  truckCareDatabase.getItems(truckListCallbackHandlers.getAllTrucksHandler);
+controller.getAllTrucks = function(req, res) {
+    truckCareDatabase.getItems(truckListCallbackHandlers.getAllTrucksHandler);
 };
 
 module.exports = controller;

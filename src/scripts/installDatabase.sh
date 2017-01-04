8552d5947f9dc44d748fbff1b82225bd7615583d
Brandon Allen <firexfighterx@gmail.com>
2017-01-04 10:38:01 -0600

    Updated install script to not use relative paths

--- src/scripts/installDatabase.sh
+++ src/scripts/installDatabase.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mysql -u root < ~/code/home/truck-care-api/src/scripts/DropTables.sql
-mysql -u root < ~/code/home/truck-care-api/src/scripts/CreateTables.sql
-mysql -u root < ~/code/home/truck-care-api/src/scripts/CreateViews.sql
-mysql -u root < ~/code/home/truck-care-api/src/scripts/DataInsert.sql
+mysql -u root < ./DropTables.sql
+mysql -u root < ./CreateTables.sql
+mysql -u root < ./CreateViews.sql
+mysql -u root < ./DataInsert.sql
